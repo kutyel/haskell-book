04bf8150c311c66a39f4e0aae649bd01c0be78c4
kutyel <flaviocorpa@gmail.com>
2018-06-21 17:34:13 +0200

    Hello, Haskell!

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,26 @@
 module Lib
-    ( someFunc
+    ( sayHello
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- Hello World!
+sayHello :: String -> IO ()
+sayHello x = putStrLn ("Hello, " ++ x ++ "!")
+
+-- First function
+triple x = x * 3
+
+-- Exercises: Comprehension Check
+
+half x = x / 2
+
+square x = x * x
+
+-- Exercises: Heal the Sick
+
+area x = pi * (x * x)
+
+double x = x * 2
+
+x = 7
+y = 10
+f = x + y
