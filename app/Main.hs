b14c11437ba24708c94d1a79d307c3fa1af92326
kutyel <flaviocorpa@gmail.com>
2018-06-22 15:19:05 +0200

    A Head Code

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,5 @@ module Main where
 
 import Lib
 
-main :: IO ()
-main = someFunc
+main :: String -> IO ()
+main = sayHello
--- src/Lib.hs
+++ src/Lib.hs
@@ -24,3 +24,25 @@ double x = x * 2
 x = 7
 y = 10
 f = x + y
+
+-- Exercises: A Head Code
+
+mult1 = x * y
+  where x = 5
+        y = 6
+
+-- 1. let x = 3; y = 1000 in x * 3 + y
+mult2 = x * 3 + y
+  where x = 3
+        y = 1000
+
+-- 2. let y = 10; x = 10 * 5 + y in x * 5
+mult3 = x * 5
+  where y = 10
+        x = 10 * 5 + y
+
+-- 3. let x = 7; y = negate x; z = y * 10 in z / x + y
+mult4 = z / x + y
+  where x = 7
+        y = negate x
+        z = y * 10
