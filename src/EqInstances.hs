2d187ddeb6076c82fca3a7ca6729a9a09ade7e6c
kutyel <flavio.corparios@ext.soprasteria.com>
2018-12-02 17:02:55 +0100

    Write the `Eq` instance for the datatype provided

---
+++ src/EqInstances.hs
@@ -0,0 +1,56 @@
+module EqInstances where
+
+-- 1
+data TisAnInteger =
+  TisAn Integer
+
+instance Eq TisAnInteger where
+  (==) (TisAn x) (TisAn x') = x == x'
+
+-- 2
+data TwoIntegers =
+  Two Integer Integer
+
+instance Eq TwoIntegers where
+  (==) (Two x y) (Two x' y') = x == x' && y == y'
+
+-- 3
+data StringOrInt =
+    TisAnInt   Int
+  | TisAString String
+
+instance Eq StringOrInt where
+  (==) (TisAnInt x) (TisAnInt x') = x == x'
+  (==) (TisAString y) (TisAString y') = y == y'
+
+-- 4
+data Pair a =
+  Pair a a
+
+instance Eq a => Eq (Pair a) where
+  (==) (Pair x _) (Pair x' _) = x == x'
+
+-- 5
+data Tuple a b =
+  Tuple a b
+
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+  (==) (Tuple x y) (Tuple x' y') = x == x' && y == y'
+
+-- 6
+data Which a =
+    ThisOne a
+  | ThatOne a
+
+instance Eq a => Eq (Which a) where
+  (==) (ThisOne x) (ThisOne x') = x == x'
+  (==) (ThatOne x) (ThatOne x') = x == x'
+
+-- 7
+data EitherOr a b =
+    Hello a
+  | Goodbye b
+
+instance (Eq a, Eq b) => Eq (EitherOr a b) where
+  (==) (Hello x) (Hello x') = x == x'
+  (==) (Goodbye x) (Goodbye x') = x == x'
