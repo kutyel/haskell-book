a349766dddddaed2d633b8af3c75951f33a5cf94
kutyel <flavio.corparios@ext.soprasteria.com>
2018-12-12 15:33:45 +0100

    Pointfree! 🐳

--- src/Chapter9.hs
+++ src/Chapter9.hs
@@ -10,15 +10,15 @@ eft :: (Eq a, Enum a) => a -> a -> [a]
 eft x y = if x == y then [x] else x : eft (succ x) y
 
 eftBool :: Bool -> Bool -> [Bool]
-eftBool x y = eft x y
+eftBool = eft
 
 eftOrd :: Ordering -> Ordering -> [Ordering]
-eftOrd x y = eft x y
+eftOrd = eft
 
 eftInt :: Int -> Int -> [Int]
-eftInt x y = eft x y
+eftInt = eft
 
 eftChar :: Char -> Char -> [Char]
-eftChar x y = eft x y
+eftChar = eft
 
 -- TODO: Comprehend Thy Lists
