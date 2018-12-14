083fdc29491a55672dddc5090c19ec231d9b6886
kutyel <flavio.corparios@ext.soprasteria.com>
2018-12-14 10:27:01 +0100

    Comprehend Thy Lists 🧐

--- src/Chapter9.hs
+++ src/Chapter9.hs
@@ -21,4 +21,20 @@ eftInt = eft
 eftChar :: Char -> Char -> [Char]
 eftChar = eft
 
--- TODO: Comprehend Thy Lists
+-- Comprehend Thy Lists
+
+mySqr :: [Integer]
+mySqr = [x^2 | x <- [1..10]] -- [1,4,9,16,25,36,49,64,81,100]
+
+q1 :: [Integer]
+q1 = [x | x <- mySqr, rem x 2 == 0] -- [4,16,36,64,100] ✅
+
+q2 :: [(Integer, Integer)]
+q2 = [(x, y) | x <- mySqr,
+               y <- mySqr,
+               x < 50, y > 50] -- [(1,64), (4,81), (9,100)] ❌ much longer!!!
+
+q3 :: [(Integer, Integer)]
+q3 = take 5 [(x, y) | x <- mySqr,
+                      y <- mySqr,
+                      x < 50, y > 50] -- [(1,64), (1,81), (1,100), (4,64), (4,81)] ❌
