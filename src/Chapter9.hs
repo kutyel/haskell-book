e4e5bf29783776f06e7b1d34efaed17a504976f8
kutyel <flavio.corparios@ext.soprasteria.com>
2018-12-14 15:24:41 +0100

    Bottoms and normal form

--- src/Chapter9.hs
+++ src/Chapter9.hs
@@ -47,3 +47,26 @@ myCube = [x^3 | x <- [1..5]]
 ex1 = length [ (x, y) | x <- mySqur,
                         y <- myCube,
                         x < 50, y < 50] -- > 15
+
+-- Bottom Madness
+
+-- 1) [x^y | x <- [1..5], y <- [2, undefined]] > bottom! ✅
+-- 2) take 1 $ [x^y | x <- [1..5], y <- [2, undefined]] > value ✅
+-- 3) sum [1, undefined, 3] > bottom! ✅
+-- 4) length [1, 2, undefined] > value ✅
+-- 5) length $ [1, 2, 3] ++ undefined > bottom! ✅
+-- 6) take 1 $ filter even [1, 2, 3, undefined] > value
+-- 7) take 1 $ filter even [1, 3, undefined] > value ❌ bottom! (no even...)
+-- 8) take 1 $ filter odd [1, 3, undefined] > value ✅
+-- 9) take 2 $ filter odd [1, 3, undefined] > value ✅
+-- 10) take 3 $ filter odd [1, 3, undefined] > bottom! ✅
+
+-- Is it in normal form?
+
+-- 1) [1, 2, 3, 4, 5] -> NF
+-- 2) 1 : 2 : 3 : 4 : _ -> WHNF
+-- 3) enumFromTo 1 10 -> neither
+-- 4) length [1, 2, 3, 4, 5] -> neither
+-- 5) sum (enumFromTo 1 10) -> WHNF
+-- 6) ['a'..'m'] ++ ['n'..'z'] -> neither
+-- 7) (_, 'b') -> WHNF
