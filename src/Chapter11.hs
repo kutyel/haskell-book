78dba22aeaf5dcaa98ec6201470d6b63fbdaa42f
kutyel <flavio.corparios@ext.soprasteria.com>
2019-01-13 00:14:15 +0100

    Map Binary Tree 🌲

--- src/Chapter11.hs
+++ src/Chapter11.hs
@@ -189,3 +189,39 @@ allProgrammers =
   | os <- allOperatingSystems
   , lang <- allLanguages
   ]
+
+-- Binary Tree
+
+data BinaryTree a =
+    Leaf
+  | Node (BinaryTree a) a (BinaryTree a)
+  deriving (Eq, Ord, Show)
+
+insert :: Ord a => a -> BinaryTree a -> BinaryTree a
+insert b Leaf = Node Leaf b Leaf
+insert b (Node left a right)
+  | b == a = Node left a right
+  | b < a  = Node (insert b left) a right
+  | b > a  = Node left a (insert b right)
+
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree _ Leaf = Leaf
+mapTree f (Node left a right) =
+  Node (mapTree f left) (f a) (mapTree f right)
+
+testTree' :: BinaryTree Integer
+testTree' =
+  Node (Node Leaf 3 Leaf)
+       1
+       (Node Leaf 4 Leaf)
+
+mapExpected =
+  Node (Node Leaf 4 Leaf)
+       2
+       (Node Leaf 5 Leaf)
+
+-- acceptance test for mapTree
+mapOkay =
+  if mapTree (+1) testTree' == mapExpected
+  then print "yup okay!"
+  else error "test failed!"
