1bcd0ca2afe0d0f04714a3b85e2433260add4b85
kutyel <flavio.corparios@ext.soprasteria.com>
2019-01-15 00:10:20 +0100

    The Quad ⨌

--- src/Chapter11.hs
+++ src/Chapter11.hs
@@ -190,6 +190,85 @@ allProgrammers =
   , lang <- allLanguages
   ]
 
+-- The Quad
+
+data Quantum =
+    Yes
+  | No
+  | Both
+  deriving (Eq, Show)
+
+convert1 :: Quantum -> Bool
+convert1 Yes  = True
+convert1 No   = True
+convert1 Both = True
+
+convert2 :: Quantum -> Bool
+convert2 Yes  = True
+convert2 No   = True
+convert2 Both = False
+
+convert3 :: Quantum -> Bool
+convert3 Yes  = True
+convert3 No   = False
+convert3 Both = False
+
+convert4 :: Quantum -> Bool
+convert4 Yes  = True
+convert4 No   = False
+convert4 Both = True
+
+convert5 :: Quantum -> Bool
+convert5 Yes  = False
+convert5 No   = True
+convert5 Both = False
+
+convert6 :: Quantum -> Bool
+convert6 Yes  = False
+convert6 No   = False
+convert6 Both = True
+
+convert7 :: Quantum -> Bool
+convert7 Yes  = False
+convert7 No   = True
+convert7 Both = True
+
+convert8 :: Quantum -> Bool
+convert8 Yes  = False
+convert8 No   = False
+convert8 Both = False
+
+data Quad =
+    One
+  | Two
+  | Three
+  | Four
+  deriving (Eq, Show)
+
+-- 1)
+eQuad :: Either Quad Quad
+eQuad = undefined -- (4 + 4) = 8
+
+-- 2)
+prodQuad :: (Quad, Quad)
+prodQuad = undefined -- (4 * 4) = 16
+
+-- 3)
+funcQuad :: Quad -> Quad
+funcQuad = undefined -- (4 ^ 4) = 256
+
+-- 4)
+prodTBool :: (Bool, Bool, Bool)
+prodTBool = undefined -- (2 * 2 * 2) = 8
+
+-- 5)
+gTwo :: Bool -> Bool -> Bool
+gTwo = undefined -- ((2 ^ 2) ^ 2) = 16
+
+-- 6)
+fTwo :: Bool -> Quad -> Quad
+fTwo = undefined -- ((2 ^ 4) ^ 4) = 65.536
+
 -- Binary Tree
 
 data BinaryTree a =
