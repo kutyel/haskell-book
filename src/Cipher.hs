8003c750aaef524749c0899a996b2f386169991c
kutyel <flaviocorpa@gmail.com>
2019-03-25 09:44:13 +0100

    Vigenere cipher only for equal length

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -2,6 +2,7 @@ module Cipher where
 
 import Data.Bool (bool)
 import Data.Char
+import Data.List.Index (indexed)
 
 cipher :: Int -> Char -> Char
 cipher n c = chr $ mod (ord c - a + n) 26 + a
@@ -12,3 +13,10 @@ toCaesar = map $ cipher 3
 
 unCaesar :: String -> String
 unCaesar = map $ cipher (-3)
+
+getShift :: Char -> Int
+getShift x = ord x - a
+  where a = ord $ bool 'a' 'A' (isUpper x)
+
+toVigenere :: String -> String -> String
+toVigenere key = map (\(i, x) -> cipher (getShift (key !! i)) x) . indexed
