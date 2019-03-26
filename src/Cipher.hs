2d14d29dcaa95caef8fd07c17a3d064e2c81b3c7
kutyel <flaviocorpa@gmail.com>
2019-03-26 09:38:15 +0100

    Refactor Cipher 🧮

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -1,20 +1,21 @@
 module Cipher where
 
-import Data.Bool (bool)
 import Data.Char (chr, ord, isUpper)
 
+offset :: Char -> Int
+offset x = ord $ if isUpper x then 'A' else 'a'
+
+shift :: Char -> Int
+shift x = ord x - offset x
+
 cipher :: Int -> Char -> Char
-cipher n c = chr $ mod (ord c - a + n) 26 + a
-  where a = ord $ bool 'a' 'A' (isUpper c)
+cipher n c = chr $ mod (shift c + n) 26 + offset c
 
 toCaesar :: String -> String
 toCaesar = map $ cipher 3
 
-unCaesar :: String -> String
-unCaesar = map $ cipher (-3)
-
-shift :: Char -> Int
-shift x = ord x - (ord $ bool 'a' 'A' (isUpper x))
+fromCaesar :: String -> String
+fromCaesar = map $ cipher (-3)
 
 toVigenere :: String -> String -> String
 toVigenere secret = zipWith (cipher . shift) (cycle secret) . concat . words
