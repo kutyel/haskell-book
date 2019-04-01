a7aa4960dca0129c7649861a39a023e730412674
kutyel <flaviocorpa@gmail.com>
2019-04-01 15:00:51 +0200

    Hutton's Razor! ⚔️

---
+++ src/Hutton.hs
@@ -0,0 +1,17 @@
+module Hutton where
+
+data Expr
+  = Lit Integer
+  | Add Expr Expr
+
+-- 1
+
+eval :: Expr -> Integer
+eval (Lit x) = x
+eval (Add x y) = eval x + eval y
+
+-- 2
+
+printExpr :: Expr -> String
+printExpr (Lit x) = show x
+printExpr (Add x y) = printExpr x ++ " + " ++ printExpr y
