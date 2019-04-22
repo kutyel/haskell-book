93035b23d874c1db9de0096a4ff3f358eb303f6c
kutyel <flaviocorpa@gmail.com>
2019-04-22 12:57:15 +0200

    Validate the word ✅

--- src/Chapter12.hs
+++ src/Chapter12.hs
@@ -1,26 +1,28 @@
 module Chapter12 where
 
 import Data.Char (toLower)
+import Data.List (partition)
 
 -- Determine the kinds
-
 -- 1) id :: a -> a ? * -> *
 -- 2) r :: a -> f a ? a = *, f = * -> *
-
 -- String processing
-
 -- 1
 notThe :: String -> Maybe String
-notThe s = if s == "the" then Nothing else Just s
+notThe s =
+  if s == "the"
+    then Nothing
+    else Just s
 
 replaceThe :: String -> String
 replaceThe = unwords . (map replace) . words
-  where replace x = case notThe x of
-          Nothing -> "a"
-          Just x  -> x
+  where
+    replace x =
+      case notThe x of
+        Nothing -> "a"
+        Just x -> x
 
 -- 2
-
 isVowel :: Char -> Bool
 isVowel = flip elem "aeiou" . toLower
 
@@ -29,10 +31,27 @@ startsWithVowel = isVowel . head
 
 countTheBeforeVowel :: String -> Integer
 countTheBeforeVowel str = go (words str) 0
-  where go (_:[]) count = count
-        go (x:y:xs) count = go (y:xs) (if x == "the" && startsWithVowel y then count + 1 else count)
+  where
+    go (_:[]) count = count
+    go (x:y:xs) count =
+      go
+        (y : xs)
+        (if x == "the" && startsWithVowel y
+           then count + 1
+           else count)
 
 -- 3
-
 countVowels :: String -> Integer
 countVowels = fromIntegral . length . filter isVowel
+
+-- validate the word
+newtype Word' =
+  Word' String
+  deriving (Eq, Show)
+
+mkWord :: String -> Maybe Word'
+mkWord s
+  | length vs > length cs = Nothing
+  | otherwise = Just (Word' s)
+  where
+    (vs, cs) = partition isVowel s
