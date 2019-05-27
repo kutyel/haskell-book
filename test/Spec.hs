abe1de52ff3e23b1e7fb66e6aea5f4e221c1926b
kutyel <flaviocorpa@gmail.com>
2019-05-27 14:19:00 +0200

    Prepare for multiple tests! 🧪

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,5 @@
-import           Chapter8        (dividedBy, mult)
+import qualified Test.Chapter8 as Chapter8
 import           Test.Hspec
-import           Test.QuickCheck
 
 main :: IO ()
-main =
-  hspec $ do
-    describe "Haskell Book tests " $ do
-      it "1 + 1 is greater than 1" $ do (1 + 1) > 1 `shouldBe` True
-      it "2 + 2 is equal to 4" $ do 2 + 2 `shouldBe` 4
-      -- property testing!
-      it "x + 1 is always greater than x" $ do
-        property $ \x -> x + 1 > (x :: Int)
-      it "dividedBy should work" $ do
-        dividedBy 15 3 `shouldBe` (5, 0)
-        dividedBy 22 5 `shouldBe` (4, 2)
-      it "mult by recursive summation should work" $ do
-        mult 7 3 `shouldBe` 7 * 3
-        mult 5 5 `shouldBe` 5 * 5
-        mult 4 3 `shouldBe` 4 * 3
+main = hspec $ do Chapter8.spec
