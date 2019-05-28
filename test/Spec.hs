6b46563aaac151453ae7c5091dab6a2ec37daa00
kutyel <flaviocorpa@gmail.com>
2019-05-28 08:34:12 +0200

    Test WordNumber 🔢

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,9 @@
-import qualified Test.Chapter8 as Chapter8
+import qualified Test.Chapter8   as Chapter8
 import           Test.Hspec
+import qualified Test.WordNumber as WordNumber
 
 main :: IO ()
-main = hspec $ do Chapter8.spec
+main =
+  hspec $ do
+    Chapter8.spec
+    WordNumber.spec
--- test/Test/Chapter8.hs
+++ test/Test/Chapter8.hs
@@ -6,7 +6,7 @@ import           Test.QuickCheck
 
 spec :: Spec
 spec = do
-  describe "Arithmetic " $ do
+  describe "Chapter8" $ do
     it "1 + 1 is greater than 1" $ do (1 + 1) > 1 `shouldBe` True
     it "2 + 2 is equal to 4" $ do 2 + 2 `shouldBe` 4
     -- property testing!
