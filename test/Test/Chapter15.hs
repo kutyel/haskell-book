7c6f0781a0dd70a5f30e30666064b61bfea99efa
kutyel <flaviocorpa@gmail.com>
2019-06-11 18:36:36 +0200

    Implement and test First' Monoid 🤯

--- test/Test/Chapter15.hs
+++ test/Test/Chapter15.hs
@@ -1,5 +1,7 @@
 module Test.Chapter15 where
 
+import           Chapter15       (Optional (..))
+import           Control.Monad   (liftM)
 import           Data.Monoid
 import           Test.Hspec
 import           Test.QuickCheck
@@ -18,6 +20,26 @@ instance Semigroup Bull where
 instance Monoid Bull where
   mempty = Fools
 
+newtype First' a =
+  First'
+    { getFirst' :: Optional a
+    }
+  deriving (Eq, Show)
+
+instance Semigroup (First' a) where
+  (<>) a b =
+    case (getFirst' a, getFirst' b) of
+      (Nada, Nada) -> First' Nada
+      (Only x, _)  -> First' (Only x)
+      (_, Only x)  -> First' (Only x)
+
+instance Monoid (First' a) where
+  mempty = First' Nada
+
+instance Arbitrary a => Arbitrary (First' a) where
+  arbitrary =
+    frequency [(1, return $ First' Nada), (3, liftM (First' . Only) arbitrary)]
+
 monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
 monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
 
@@ -27,16 +49,25 @@ monoidLeftIdentity a = (mempty <> a) == a
 monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
 monoidRightIdentity a = (a <> mempty) == a
 
+firstMappend :: First' a -> First' a -> First' a
+firstMappend = mappend
+
+type FirstStr = First' String
+
 spec :: Spec
 spec = do
-  let ma = monoidAssoc
-      mli = monoidLeftIdentity
-      mri = monoidRightIdentity
   describe "Monoids" $ do
     describe "Bull" $ do
       it "monoid associativity should work" $
-        property (ma :: Bull -> Bull -> Bull -> Bool)
+        property (monoidAssoc :: Bull -> Bull -> Bull -> Bool)
       it "monoid left identity should fail" $
-        expectFailure $ property (mli :: Bull -> Bool)
+        expectFailure $ property (monoidLeftIdentity :: Bull -> Bool)
       it "monoid right identity should fail" $
-        expectFailure $ property (mri :: Bull -> Bool)
+        expectFailure $ property (monoidRightIdentity :: Bull -> Bool)
+    describe "First'" $ do
+      it "monoid associativity should work" $
+        property (monoidAssoc :: FirstStr -> FirstStr -> FirstStr -> Bool)
+      it "monoid left identity should work" $
+        property (monoidLeftIdentity :: FirstStr -> Bool)
+      it "monoid right identity should work" $
+        property (monoidRightIdentity :: FirstStr -> Bool)
