cfc19571e224c1f6e9b7c01f1efede9c68ec8f00
kutyel <flaviocorpa@gmail.com>
2019-07-02 12:17:13 +0200

    Back to hfmt λ

--- src/Chapter15.hs
+++ src/Chapter15.hs
@@ -9,11 +9,12 @@ data Optional a
   deriving (Eq, Show)
 
 instance Semigroup a => Semigroup (Optional a) where
-  (<>) a b = case (a, b) of
-    (Nada  , Nada  ) -> Nada
-    (Only x, Nada  ) -> Only x
-    (Nada  , Only x) -> Only x
-    (Only x, Only y) -> Only (x <> y)
+  (<>) a b =
+    case (a, b) of
+      (Nada, Nada)     -> Nada
+      (Only x, Nada)   -> Only x
+      (Nada, Only x)   -> Only x
+      (Only x, Only y) -> Only (x <> y)
 
 instance Monoid a => Monoid (Optional a) where
   mempty = Nada
@@ -28,16 +29,17 @@ type Noun = String
 type Exclamation = String
 
 madlibbin :: Exclamation -> Adverb -> Noun -> Adjective -> String
-madlibbin e adv noun adj = mconcat
-  [ e
-  , "! he said "
-  , adv
-  , " as he jumped into his car "
-  , noun
-  , " and drove off with his "
-  , adj
-  , " wife."
-  ]
+madlibbin e adv noun adj =
+  mconcat
+    [ e
+    , "! he said "
+    , adv
+    , " as he jumped into his car "
+    , noun
+    , " and drove off with his "
+    , adj
+    , " wife."
+    ]
 
 -- Combine
 newtype Combine a b =
@@ -70,11 +72,12 @@ data Validation a b
   deriving (Eq, Show)
 
 instance Semigroup a => Semigroup (Validation a b) where
-  a <> b = case (a, b) of
-    (Success x, Failure _) -> Success x
-    (Failure _, Success x) -> Success x
-    (Success x, Success _) -> Success x
-    (Failure x, Failure y) -> Failure (x <> y)
+  a <> b =
+    case (a, b) of
+      (Success x, Failure _) -> Success x
+      (Failure _, Success x) -> Success x
+      (Success x, Success _) -> Success x
+      (Failure x, Failure y) -> Failure (x <> y)
 
 -- Mem
 newtype Mem s a =
@@ -83,10 +86,11 @@ newtype Mem s a =
     }
 
 instance Semigroup a => Semigroup (Mem s a) where
-  Mem f <> Mem g = Mem $ \x ->
-    let (a, b) = g x
-        (c, d) = f b
-    in  (a <> c, d)
+  Mem f <> Mem g =
+    Mem $ \x ->
+      let (a, b) = g x
+          (c, d) = f b
+       in (a <> c, d)
 
 instance Monoid a => Monoid (Mem s a) where
   mempty = Mem $ \x -> (mempty, x)
@@ -96,9 +100,9 @@ main = do
       failure = Failure
       success :: Int -> Validation String Int
       success = Success
-      f'      = Mem $ \s -> ("hi", s + 1)
-      rmzero  = runMem mempty 0
-      rmleft  = runMem (f' <> mempty) 0
+      f' = Mem $ \s -> ("hi", s + 1)
+      rmzero = runMem mempty 0
+      rmleft = runMem (f' <> mempty) 0
       rmright = runMem (mempty <> f') 0
   print $ success 1 <> failure "blah" -- Success 1
   print $ failure "woot" <> failure "blah" -- Failure "wootblah"
