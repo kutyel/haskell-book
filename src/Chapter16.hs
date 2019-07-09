a47b3630fc378358b78c31895dc25268e5955486
kutyel <flaviocorpa@gmail.com>
2019-07-09 13:49:37 +0200

    Chapter exercises with Functors!

--- src/Chapter16.hs
+++ src/Chapter16.hs
@@ -1,5 +1,7 @@
 module Chapter16 where
 
+import           GHC.Arr
+
 -- 1) What is the kind of a? *
 -- f :: a -> a
 -- 2) What are the kinds of b and T? * -> *
@@ -44,3 +46,54 @@ data Sum a b
 instance Functor (Sum a) where
   fmap _ (First x)  = First x
   fmap f (Second x) = Second (f x)
+
+-- Chapter exercises!
+-- 1) 🚫 Cannot be a Functor, kind = *
+-- 2) ✅ yes, kind = * -> *
+data BoolAndSomethingElse a
+  = False' a
+  | True a
+
+-- 3) ✅ yes, kind = * -> *
+data BoolAndMaybeSomethingElse a
+  = Falsish
+  | Truish a
+
+-- 4) ✅ yes, kind = (* -> *) -> *, but given * will be: * -> *
+newtype Mu f =
+  InF
+    { outF :: f (Mu f)
+    }
+
+-- 5) 🚫 Cannot be a Functor, kind = *
+data D =
+  D (Array Word Word) Int Int
+
+-- Rearrange Functors!
+-- 1)
+data Sum' b a
+  = First' a
+  | Second' b
+
+instance Functor (Sum' e) where
+  fmap f (First' a)  = First' (f a)
+  fmap f (Second' b) = Second' b
+
+-- 2)
+data Company a c b
+  = DeepBlue a c
+  | Something b
+
+instance Functor (Company e e') where
+  fmap f (Something b)  = Something (f b)
+  fmap _ (DeepBlue a c) = DeepBlue a c
+
+-- 3)
+data More b a
+  = L a b a
+  | R b a b
+  deriving (Eq, Show)
+
+instance Functor (More x) where
+  fmap f (L a b a') = L (f a) b (f a')
+  fmap f (R b a b') = R b (f a) b'
