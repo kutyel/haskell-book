f09b1d6bcc13bd01fa38aef09d6f9eb7a83f90dd
kutyel <flaviocorpa@gmail.com>
2019-07-10 15:34:13 +0200

    More functors and goats! 🐐

--- src/Chapter16.hs
+++ src/Chapter16.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Chapter16 where
 
 import           GHC.Arr
@@ -97,3 +99,54 @@ data More b a
 instance Functor (More x) where
   fmap f (L a b a') = L (f a) b (f a')
   fmap f (R b a b') = R b (f a) b'
+
+-- write functor instances!
+-- 1)
+data Quant a b
+  = Finance
+  | Desk a
+  | Bloor b
+
+instance Functor (Quant e) where
+  fmap _ Finance   = Finance
+  fmap _ (Desk x)  = Desk x
+  fmap f (Bloor y) = Bloor (f y)
+
+-- 2)
+data K a b =
+  K a
+
+instance Functor (K e) where
+  fmap _ (K x) = K x
+
+-- 3)
+newtype Flip f a b =
+  Flip (f b a)
+  deriving (Eq, Show)
+
+newtype K' a b =
+  K' a
+
+instance Functor (Flip K' a) where
+  fmap f (Flip (K' x)) = Flip $ K' (f x)
+
+-- 4)
+data EvilGoateeConst a b =
+  GoatyConst b
+
+instance Functor (EvilGoateeConst e) where
+  fmap f (GoatyConst x) = GoatyConst (f x)
+
+-- 5)
+data LiftItOut f a =
+  LiftItOut (f a)
+
+instance Functor f => Functor (LiftItOut f) where
+  fmap f (LiftItOut x) = LiftItOut (fmap f x)
+
+-- 6)
+data Parappa f g a =
+  DaWrappa (f a) (g a)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+  fmap f (DaWrappa g x) = DaWrappa (fmap f g) (fmap f x)
