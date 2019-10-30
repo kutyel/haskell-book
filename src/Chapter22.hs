566db8f25d3f21fbfd2ca3921095d00d2d8b47af
kutyel <flaviocorpa@gmail.com>
2019-10-30 09:45:10 +0100

    Finish Chapter 22 (Reader) 💪🏼

--- src/Chapter22.hs
+++ src/Chapter22.hs
@@ -4,6 +4,7 @@ module Chapter22 where
 
 import           Control.Monad (join)
 import           Data.Char
+import           Data.Maybe    (fromMaybe)
 
 newtype Reader r a =
   Reader
@@ -91,3 +92,58 @@ data Dog =
 
 getDogRM :: Reader Person Dog
 getDogRM = Reader (Dog <$> dogName <*> address)
+
+-- Chapter Exercises
+x = [1, 2, 3]
+
+y = [4, 5, 6]
+
+z = [7, 8, 9]
+
+xs :: Maybe Integer
+xs = lookup 3 $ zip x y
+
+ys :: Maybe Integer
+ys = lookup 6 $ zip y z
+
+zs :: Maybe Integer
+zs = lookup 4 $ zip x y
+
+z' :: Integer -> Maybe Integer
+z' n = lookup n $ zip x z
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = (,) <$> ys <*> zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 = (,) <$> z' <*> z'
+
+summed :: Num c => (c, c) -> c
+summed = uncurry (+)
+
+bolt :: Integer -> Bool
+bolt = (&&) <$> (> 3) <*> (< 8)
+
+sequA :: Integral a => a -> [Bool]
+sequA = sequenceA [(> 3), (< 8), even]
+
+s' :: Maybe Integer
+s' = summed <$> ((,) <$> xs <*> ys)
+
+main :: IO ()
+main = do
+  print $ sequenceA [Just 3, Just 2, Just 1]
+  print $ sequenceA [x, y]
+  print $ sequenceA [xs, ys]
+  print $ summed <$> ((,) <$> xs <*> ys)
+  print $ fmap summed ((,) <$> xs <*> zs)
+  print $ bolt 7
+  print $ fmap bolt z
+  print $ sequenceA [(> 3), (< 8), even] 7
+  -- exercises
+  print $ and $ sequA 6
+  print $ sequA $ fromMaybe 0 s'
+  print $ bolt $ fromMaybe 0 ys
