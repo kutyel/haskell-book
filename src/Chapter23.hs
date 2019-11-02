b98c119595e2e1d04a883fb57ec3a2331f1e544f
kutyel <flaviocorpa@gmail.com>
2019-11-02 18:28:15 +0100

    Start Chapter 23 exercises! (State Monad)

---
+++ src/Chapter23.hs
@@ -0,0 +1,54 @@
+module Chapter23 where
+
+import           System.Random
+
+data Die
+  = DieOne
+  | DieTwo
+  | DieThree
+  | DieFour
+  | DieFive
+  | DieSix
+  deriving (Eq, Show)
+
+intToDie :: Int -> Die
+intToDie n =
+  case n of
+    1 -> DieOne
+    2 -> DieTwo
+    3 -> DieThree
+    4 -> DieFour
+    5 -> DieFive
+    6 -> DieSix
+    x -> error $ "intToDie got non 1-6 integer: " ++ show x
+
+rollsToGetTwenty :: StdGen -> Int
+rollsToGetTwenty = go 0 0
+  where
+    go :: Int -> Int -> StdGen -> Int
+    go sum count gen
+      | sum >= 20 = count
+      | otherwise =
+        let (die, nextGen) = randomR (1, 6) gen
+         in go (sum + die) (count + 1) nextGen
+
+-- exercises: roll your own
+rollsToGetN :: Int -> StdGen -> Int
+rollsToGetN n = go 0 0
+  where
+    go :: Int -> Int -> StdGen -> Int
+    go sum count gen
+      | sum >= n = count
+      | otherwise =
+        let (die, nextGen) = randomR (1, 6) gen
+         in go (sum + die) (count + 1) nextGen
+
+rollsCountLogged :: Int -> StdGen -> (Int, [Die])
+rollsCountLogged n = go 0 (0, [])
+  where
+    go :: Int -> (Int, [Die]) -> StdGen -> (Int, [Die])
+    go sum count@(c, cs) gen
+      | sum >= n = count
+      | otherwise =
+        let (die, nextGen) = randomR (1, 6) gen
+         in go (sum + die) (c + 1, intToDie die : cs) nextGen
