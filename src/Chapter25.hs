7bd9cd398d4f65d7a913db6e28186480ca3464a6
Flavio Corpa <flavio.corpa@47deg.com>
2020-01-02 12:07:12 +0100

    Finish Chapter 25! 🧨

--- src/Chapter25.hs
+++ src/Chapter25.hs
@@ -29,7 +29,60 @@ instance (Foldable f, Foldable g) => Foldable (Compose f g) where
 instance (Traversable f, Traversable g) => Traversable (Compose f g) where
   traverse f (Compose fga) = Compose <$> (traverse . traverse) f fga
 
--- TODO: Bifunctors here
+-- Bifunctors here
+class Bifunctor p where
+
+  {-# MINIMAL bimap | first, second #-}
+
+  bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+  bimap f g = first f . second g
+
+  first :: (a -> b) -> p a c -> p b c
+  first f = bimap f id
+
+  second :: (b -> c) -> p a b -> p a c
+  second = bimap id
+
+-- 1)
+data Deux a b = Deux a b
+
+instance Bifunctor Deux where
+  bimap f g (Deux x y) = Deux (f x) (g y)
+
+-- 2)
+newtype Const a b = Const a
+
+instance Bifunctor Const where
+  bimap f _ (Const x) = Const $ f x
+
+-- 3)
+data Drei a b c = Drei a b c
+
+instance Bifunctor (Drei a) where
+  bimap f g (Drei x y z) = Drei x (f y) (g z)
+
+-- 4)
+data SuperDrei a b c = SuperDrei a b
+
+instance Bifunctor (SuperDrei a) where
+  bimap f _ (SuperDrei x y) = SuperDrei x (f y)
+
+-- 5)
+newtype SemiDrei a b c = SemiDrei a
+
+instance Bifunctor (SemiDrei a) where
+  bimap _ _ (SemiDrei x) = SemiDrei x
+
+-- 6)
+data Quadriceps a b c d = Quadzz a b c d
+
+instance Bifunctor (Quadriceps a b) where
+  bimap f g (Quadzz x y z z') = Quadzz x y (f z) (g z')
+
+-- 7)
+instance Bifunctor Either where
+  bimap f _ (Left x) = Left $ f x
+  bimap _ g (Right x) = Right $ g x
 
 -- my first Monad Transformer
 newtype IdentityT f a
