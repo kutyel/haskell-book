faeb1eee43b783ef3b8bb5473d142bf5ec21b14b
Flavio Corpa <flavio.corpa@47deg.com>
2020-03-05 14:38:32 +0100

    Fool around with :sprint

--- src/Chapter27.hs
+++ src/Chapter27.hs
@@ -95,6 +95,22 @@ repeat' x = xs where xs = Cons x xs
 main :: IO ()
 main = print $ take' 10 $ map' (+ 1) (repeat' 1)
 
+-- What will :sprint output?
+
+-- 1) let x = 1 -> x = _
+
+-- 2) let x = ['1'] -> x = "1"
+
+-- 3) let x = [1] -> x = _
+
+-- 4) let x = 1 :: Int -> x = 1
+
+-- 5) let f = \x -> x
+--    let x = f 1 => x = _
+
+-- 5) let f :: Int -> Int; f = \x -> x
+--    let x = f 1 => x = _
+
 -- Make the expression bottom
 
 bot :: IO ()
