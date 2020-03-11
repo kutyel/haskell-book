01408faef8affeab9f91d7e9d0be198282f5b1f8
Flavio Corpa <flavio.corpa@47deg.com>
2020-03-11 16:08:36 +0100

    Add bench on Maps vs Lists (key access) 🔐

--- src/Chapter28.hs
+++ src/Chapter28.hs
@@ -3,6 +3,7 @@ module Chapter28 where
 -- Basic libraries
 
 import Criterion.Main
+import qualified Data.Map as M
 
 -- safe access operator
 infixl 0 !?
@@ -25,10 +26,28 @@ xs !? n
 myList :: [Int]
 myList = [1 .. 9999]
 
+-- Maps
+
+genList :: Int -> [(String, Int)]
+genList n = go n []
+  where
+    go 0 xs = ("0", 0) : xs
+    go n' xs =
+      go (n' - 1) ((show n', n') : xs)
+
+pairList :: [(String, Int)]
+pairList = genList 9001
+
+testMap :: M.Map String Int
+testMap = M.fromList pairList
+
 main :: IO ()
 main =
   defaultMain
     [ -- benchmark index access (unsafe/safe)
       bench "index list 9999" $ whnf (myList !!) 9998,
-      bench "index list maybe index" $ whnf (myList !?) 998
+      bench "index list maybe index" $ whnf (myList !?) 9998,
+      -- benchmark maps against lists
+      bench "lookup one thing, list" $ whnf (lookup "notfound") pairList,
+      bench "lookup one thing, map" $ whnf (M.lookup "notfound") testMap
     ]
