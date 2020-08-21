4b2b2846a5b6c41faff766705be373d397eaccab
Flavio Corpa <flavio.corpa@47deg.com>
2020-08-21 12:51:37 +0200

    Add files for chapters 29 and 30! 🧨

---
+++ src/Chapter29.hs
@@ -0,0 +1,51 @@
+module Chapter29 where
+
+import Data.Char (chr, isUpper, ord)
+import System.IO
+
+offset :: Char -> Int
+offset x = ord $ if isUpper x then 'A' else 'a'
+
+shift :: Char -> Int
+shift x = ord x - offset x
+
+cipher :: Int -> Char -> Char
+cipher n c = chr $ mod (shift c + n) 26 + offset c
+
+toVigenere :: String -> String -> String
+toVigenere secret = zipWith (cipher . shift) (cycle secret) . concat . words
+
+fromVigenere :: String -> String -> String
+fromVigenere secret =
+  zipWith (cipher . negate . shift) (cycle secret) . concat . words
+
+-- IO
+
+getInput :: IO String
+getInput = do
+  hSetBuffering stdout NoBuffering
+  putStrLn "Enter a secret:"
+  getLine
+
+toVigenereIO :: IO ()
+toVigenereIO = do
+  secret <- getInput
+  putStrLn "Enter a message to encrypt:"
+  message <- getLine
+  putStrLn $ toVigenere secret message
+
+fromVigenereIO :: IO ()
+fromVigenereIO = do
+  secret <- getInput
+  putStrLn "Enter a message to decrypt:"
+  message <- getLine
+  putStrLn $ fromVigenere secret message
+
+-- TODO: Chapter 29 Exercises -----------------------------
+-- Make an executable that takes a key and a mode argument:
+--  * If the mode is -d, the executable decrypts the input from standard in
+--    and writes the decrypted text to standard out.
+--  * If the mode is -e, the executable blocks on input from standard input (stdin)
+--    and writes the encrypted output to standard output (stdout).
+main :: IO ()
+main = putStrLn "please complete this exercise!"
---
+++ src/Chapter30.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE GADTs #-}
+
+module Chapter30 where
+
+import Control.Exception (ArithException (..), AsyncException (..))
+import Data.Typeable
+
+data MyException
+  = forall e. (Show e, Typeable e) => MyException e
+
+instance Show MyException where
+  showsPrec p (MyException e) = showsPrec p e
+
+multiError :: Int -> Either MyException Int
+multiError n =
+  case n of
+    0 -> Left (MyException DivideByZero)
+    1 -> Left (MyException StackOverflow)
+    _ -> Right n
+
+data SomeError
+  = Arith ArithException
+  | Async AsyncException
+  | SomethingElse
+  deriving (Show)
+
+discriminateError :: MyException -> SomeError
+discriminateError (MyException e) =
+  case cast e of
+    Just arith -> Arith arith
+    Nothing ->
+      case cast e of
+        Just async -> Async async
+        Nothing -> SomethingElse
+
+runDisc :: Int -> SomeError
+runDisc n = either discriminateError (const SomethingElse) (multiError n)
