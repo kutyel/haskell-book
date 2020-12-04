5497dc51859beb662a1346ffd51e30718ac941de
Flavio Corpa <flavio.corpa@47deg.com>
2020-12-04 10:13:58 +0100

    Add local nix package...

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ let
   nix-pre-commit-hooks = import (builtins.fetchTarball "https://github.com/cachix/pre-commit-hooks.nix/tarball/master");
  in {
   project = pkgs.haskellPackages.callPackage ./pkg.nix { };
-  hpc-coveralls = pkgs.haskellPackages.callPackage ./hpc-coveralls { };
+  hpc-coveralls = pkgs.haskellPackages.callPackage ./hpc-coveralls.nix { };
   pre-commit-check = nix-pre-commit-hooks.run {
     src = ./.;
     hooks = {
